b09ba102e8662ff17ea30511c7b7526792e85e6a
Ross D. Williams <34747165+rossdwilliams@users.noreply.github.com>
2021-04-30 12:51:16 +0200

    updated codeToRun
    removed loop, added andromeda options

--- validationPackage/EhdenRaPredictionValidation/extras/codeToRun.R
+++ validationPackage/EhdenRaPredictionValidation/extras/codeToRun.R
@@ -1,5 +1,7 @@
 library(EhdenRaPredictionValidation)
 
+#set location for storing temporary files
+options(andromedaTempFolder = <set andromeda temp directory>)
 # the location to save the prediction models results to:
 outputFolder <- './EhdenRaPredictionValidationResults'
 
@@ -16,55 +18,51 @@ connectionDetails <- DatabaseConnector::createConnectionDetails(dbms = dbms,
                                                                 password = pw,
                                                                 port = port)
 
-databaseNames <- c()
-databaseSchemas <- c()
+# add details of your database setting:
+cdmDatabaseName <- cdmDatabaseName
 
-for (i in 1:length(databaseNames)){
-  # add details of your database setting:
-  databaseName <- databaseNames[i]
+# add the cdm database schema with the data
+cdmDatabaseSchema <- databaseSchemas
 
-  # add the cdm database schema with the data
-  cdmDatabaseSchema <- databaseSchemas[i]
+# add the work database schema this requires read/write privileges
+cohortDatabaseSchema <- 'work database schema'
 
-  # add the work database schema this requires read/write privileges
-  cohortDatabaseSchema <- 'work database schema'
+# if using oracle please set the location of your temp schema
+oracleTempSchema <- NULL
 
-  # if using oracle please set the location of your temp schema
-  oracleTempSchema <- NULL
-
-  # the name of the table that will be created in cohortDatabaseSchema to hold the cohorts
-  cohortTable <- paste0('EhdenRaPredictionValidationCohortTable' , databaseName)
+# the name of the table that will be created in cohortDatabaseSchema to hold the cohorts
+cohortTable <- 'EhdenRaPredictionValidationCohortTable'
 
 
-  # Now run the study
-  EhdenRaPredictionValidation::execute(connectionDetails = connectionDetails,
-                                       databaseName = databaseName,
-                                       cdmDatabaseSchema = cdmDatabaseSchema,
-                                       cohortDatabaseSchema = cohortDatabaseSchema,
-                                       oracleTempSchema = NULL,
-                                       cohortTable = cohortTable,
-                                       outputFolder = outputFolder,
-                                       createCohorts = T,
-                                       runValidation = T,
-                                       packageResults = F,
-                                       minCellCount = 5,
-                                       sampleSize = NULL)
-  # the results will be saved to outputFolder.  If you set this to the
-  # predictionStudyResults/Validation package then the validation results
-  # will be accessible to the shiny viewer
+# Now run the study
+EhdenRaPredictionValidation::execute(connectionDetails = connectionDetails,
+                                     cdmDatabaseName = cdmDatabaseName,
+                                     cdmDatabaseSchema = cdmDatabaseSchema,
+                                     cohortDatabaseSchema = cohortDatabaseSchema,
+                                     oracleTempSchema = NULL,
+                                     cohortTable = cohortTable,
+                                     outputFolder = outputFolder,
+                                     createCohorts = T,
+                                     runValidation = T,
+                                     packageResults = F,
+                                     minCellCount = 5,
+                                     sampleSize = NULL)
+# the results will be saved to outputFolder.  If you set this to the
+# predictionStudyResults/Validation package then the validation results
+# will be accessible to the shiny viewer
 
-  # to package the results run (run after the validation results are complete):
-  # NOTE: the minCellCount = N will remove any result with N patients or less
-  # EhdenRaPredictionValidation::execute(connectionDetails = connectionDetails,
-  #                                  databaseName = databaseName,
-  #                                  cdmDatabaseSchema = cdmDatabaseSchema,
-  #                                  cohortDatabaseSchema = cohortDatabaseSchema,
-  #                                  oracleTempSchema = oracleTempSchema,
-  #                                  cohortTable = cohortTable,
-  #                                  outputFolder = outputFolder,
-  #                                  createCohorts = F,
-  #                                  runValidation = F,
-  #                                  packageResults = T,
-  #                                  minCellCount = 5,
-  #                                  sampleSize = NULL)
-}
+# to package the results run (run after the validation results are complete):
+# NOTE: the minCellCount = N will remove any result with N patients or less
+# EhdenRaPredictionValidation::execute(connectionDetails = connectionDetails,
+#                                  databaseName = databaseName,
+#                                  cdmDatabaseSchema = cdmDatabaseSchema,
+#                                  cohortDatabaseSchema = cohortDatabaseSchema,
+#                                  oracleTempSchema = oracleTempSchema,
+#                                  cohortTable = cohortTable,
+#                                  outputFolder = outputFolder,
+#                                  createCohorts = F,
+#                                  runValidation = F,
+#                                  packageResults = T,
+#                                  minCellCount = 5,
+#                                  sampleSize = NULL)
+
